bf709430651f9a0ab0e6d14de92ef54a2d41ee01
Alumno ULSA <alumno@D521.local>
2023-03-06 14:34:48 -0700

    commit 1

---
+++ app/Http/Controllers/AlumnosController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alumno;
+
+class AlumnosController extends Controller
+{
+    public function index() {
+        $alumnos = Alumno::all();
+
+        $argumentos = array();
+        $argumentos['alumnos'] = $alumnos;
+
+        return view('alumnos.index',$argumentos);
+    }
+    public function create() {
+        $argumentos = array();
+        return view('alumnos.create', $argumentos);
+    }
+    public function store(Request $request) {
+        $nuevoAlumno = new Alumno();
+        //Las columnas de la tabla asociada 
+        //se representan mediante propiedades del objeto
+        $nuevoAlumno->nombre = $request->input('nombre');
+        $nuevoAlumno->save();
+        //hacer que nos regrese al index
+        return redirect()->route('alumnos.index');
+    }
+
+    //Estamos recibiendo parametros de ruta a traves de parametros de funcion
+    public function edit($id) {
+        $alumno = Alumno::find($id);
+        $argumentos = array();
+        $argumentos['alumno'] = $alumno;
+        return view('alumnos.edit',$argumentos);
+    }
+
+    public function update(Request $request,$id) {
+        //busca al alumno
+        $alumno = Alumno::find($id);
+        //actualiza sus datos en base a los valores del form
+        $alumno->nombre = $request->input('nombre');
+
+        $alumno->save();
+        //hacer que nos regrese al index
+        return redirect()->route('alumnos.edit',$id)
+            ->with('exito','El alumno se ha actualizado exitosamente');
+    }
+
+}
